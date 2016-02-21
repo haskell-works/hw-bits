152ef09e25f279da90081e19af2c93d2064a1e9c
John Ky <newhoggy@gmail.com>
2016-02-21 13:35:49 +1100

    Inline all the things.

--- src/HaskellWorks/Data/Succinct/Internal.hs
+++ src/HaskellWorks/Data/Succinct/Internal.hs
@@ -87,95 +87,159 @@ class Broadword a where
 
 instance Broadword Word8 where
   bwL8 = 0x01
+  {-# INLINABLE bwL8 #-}
+
   bwH8 = 0x80
+  {-# INLINABLE bwH8 #-}
 
 instance Broadword Word16 where
   bwL8 = 0x0101
+  {-# INLINABLE bwL8 #-}
+
   bwH8 = 0x8080
+  {-# INLINABLE bwH8 #-}
 
 instance Broadword Word32 where
   bwL8 = 0x01010101
+  {-# INLINABLE bwL8 #-}
+
   bwH8 = 0x80808080
+  {-# INLINABLE bwH8 #-}
 
 instance Broadword Word64 where
   bwL8 = 0x0101010101010101
+  {-# INLINABLE bwL8 #-}
+
   bwH8 = 0x8080808080808080
+  {-# INLINABLE bwH8 #-}
 
 instance BitLength Word8 where
   bitLength _ = 8
+  {-# INLINABLE bitLength #-}
 
 instance BitLength Word16 where
   bitLength _ = 16
+  {-# INLINABLE bitLength #-}
 
 instance BitLength Word32 where
   bitLength _ = 32
+  {-# INLINABLE bitLength #-}
 
 instance BitLength Word64 where
   bitLength _ = 64
+  {-# INLINABLE bitLength #-}
 
 instance TestBit Word8 where
   (.?.) w n = B.testBit w (fromIntegral n)
+  {-# INLINABLE (.?.) #-}
 
 instance TestBit Word16 where
   (.?.) w n = B.testBit w (fromIntegral n)
+  {-# INLINABLE (.?.) #-}
 
 instance TestBit Word32 where
   (.?.) w n = B.testBit w (fromIntegral n)
+  {-# INLINABLE (.?.) #-}
 
 instance TestBit Word64 where
   (.?.) w n = B.testBit w (fromIntegral n)
+  {-# INLINABLE (.?.) #-}
 
 instance PopCount Word8 where
   popCount = fromIntegral . B.popCount
+  {-# INLINABLE popCount #-}
 
 instance PopCount Word16 where
   popCount = fromIntegral . B.popCount
+  {-# INLINABLE popCount #-}
 
 instance PopCount Word32 where
   popCount = fromIntegral . B.popCount
+  {-# INLINABLE popCount #-}
 
 instance PopCount Word64 where
   popCount = fromIntegral . B.popCount
+  {-# INLINABLE popCount #-}
 
 instance BitWise Word8 where
   (.&.) = (B..&.)
+  {-# INLINABLE (.&.) #-}
+
   (.|.) = (B..|.)
+  {-# INLINABLE (.|.) #-}
+
   (.^.) = B.xor
+  {-# INLINABLE (.^.) #-}
+
   comp  = B.complement
+  {-# INLINABLE comp #-}
 
 instance BitWise Word16 where
   (.&.) = (B..&.)
+  {-# INLINABLE (.&.) #-}
+
   (.|.) = (B..|.)
+  {-# INLINABLE (.|.) #-}
+
   (.^.) = B.xor
+  {-# INLINABLE (.^.) #-}
+
   comp  = B.complement
+  {-# INLINABLE comp #-}
 
 instance BitWise Word32 where
   (.&.) = (B..&.)
+  {-# INLINABLE (.&.) #-}
+
   (.|.) = (B..|.)
+  {-# INLINABLE (.|.) #-}
+
   (.^.) = B.xor
+  {-# INLINABLE (.^.) #-}
+
   comp  = B.complement
+  {-# INLINABLE comp #-}
 
 instance BitWise Word64 where
   (.&.) = (B..&.)
+  {-# INLINABLE (.&.) #-}
+
   (.|.) = (B..|.)
+  {-# INLINABLE (.|.) #-}
+
   (.^.) = B.xor
+  {-# INLINABLE (.^.) #-}
+
   comp  = B.complement
+  {-# INLINABLE comp #-}
 
 instance Shift Word8  where
   (.<.) w n = B.shiftL w (fromIntegral n)
+  {-# INLINABLE (.<.) #-}
+
   (.>.) w n = B.shiftR w (fromIntegral n)
+  {-# INLINABLE (.>.) #-}
 
 instance Shift Word16 where
   (.<.) w n = B.shiftL w (fromIntegral n)
+  {-# INLINABLE (.<.) #-}
+
   (.>.) w n = B.shiftR w (fromIntegral n)
+  {-# INLINABLE (.>.) #-}
 
 instance Shift Word32 where
   (.<.) w n = B.shiftL w (fromIntegral n)
+  {-# INLINABLE (.<.) #-}
+
   (.>.) w n = B.shiftR w (fromIntegral n)
+  {-# INLINABLE (.>.) #-}
 
 instance Shift Word64 where
   (.<.) w n = B.shiftL w (fromIntegral n)
+  {-# INLINABLE (.<.) #-}
+
   (.>.) w n = B.shiftR w (fromIntegral n)
+  {-# INLINABLE (.>.) #-}
 
 instance LeBitRank Word8 where
   leBitRank v s0 =
@@ -187,6 +251,7 @@ instance LeBitRank Word8 where
     let r3 = (r2 .&. 0x0f) + ((r2 .>. 4) .&. 0x0f)  in
     let r4 = r3 `mod` 255                           in
     fromIntegral r4 :: Int64
+  {-# INLINABLE leBitRank #-}
 
 instance LeBitRank Word16 where
   leBitRank v s0 =
@@ -198,6 +263,7 @@ instance LeBitRank Word16 where
     let r3 = (r2 .&. 0x0f0f) + ((r2 .>. 4) .&. 0x0f0f)  in
     let r4 = r3 `mod` 255                               in
     fromIntegral r4 :: Int64
+  {-# INLINABLE leBitRank #-}
 
 instance LeBitRank Word32 where
   leBitRank v s0 =
@@ -209,6 +275,7 @@ instance LeBitRank Word32 where
     let r3 = (r2 .&. 0x0f0f0f0f) + ((r2 .>. 4) .&. 0x0f0f0f0f)  in
     let r4 = r3 `mod` 255                                       in
     fromIntegral r4 :: Int64
+  {-# INLINABLE leBitRank #-}
 
 instance LeBitRank Word64 where
   leBitRank v s0 =
@@ -220,10 +287,12 @@ instance LeBitRank Word64 where
     let r3 = (r2 .&. 0x0f0f0f0f0f0f0f0f) + ((r2 .>. 4) .&. 0x0f0f0f0f0f0f0f0f)  in
     let r4 = r3 `mod` 255                                                       in
     fromIntegral r4 :: Int64
+  {-# INLINABLE leBitRank #-}
 
 -- TODO: Implement NOT interms of select for word-16
 instance LeBitSelect Word8 where
   leBitSelect v = leBitSelect (fromIntegral v :: Word16)
+  {-# INLINABLE leBitSelect #-}
 
 -- TODO: Remove redundant code to optimise
 instance LeBitSelect Word16 where
@@ -255,6 +324,7 @@ instance LeBitSelect Word16 where
     let t5 =      (v .>. fromIntegral (s5 - 1))  .&. 0x1                        in
     let s6 = s5 - ((t5 - r5) .&. 256) .>. 8                                     in
     fromIntegral s6
+  {-# INLINABLE leBitSelect #-}
 
 -- TODO: Remove redundant code to optimise
 instance LeBitSelect Word32 where
@@ -287,6 +357,7 @@ instance LeBitSelect Word32 where
     let t5 =      (v .>. fromIntegral (s5 - 1))  .&. 0x1                        in
     let s6 = s5 - ((t5 - r5) .&. 256) .>. 8                                     in
     fromIntegral s6
+  {-# INLINABLE leBitSelect #-}
 
 instance LeBitSelect Word64 where
   leBitSelect v rn =
@@ -319,6 +390,7 @@ instance LeBitSelect Word64 where
     let t5 =      (v .>. fromIntegral (s5 - 1))  .&. 0x1                        in
     let s6 = s5 - ((t5 - r5) .&. 256) .>. 8                                     in
     fromIntegral s6
+  {-# INLINABLE leBitSelect #-}
 
 instance BeBitRank Word8 where
   beBitRank v s0 =
@@ -330,6 +402,7 @@ instance BeBitRank Word8 where
     let r3 = (r2 .&. 0x0f) + ((r2 .>. 4) .&. 0x0f)  in
     let r4 = r3 `mod` 255                           in
     fromIntegral r4 :: Int64
+  {-# INLINABLE beBitRank #-}
 
 instance BeBitRank Word16 where
   beBitRank v s0 =
@@ -341,6 +414,7 @@ instance BeBitRank Word16 where
     let r3 = (r2 .&. 0x0f0f) + ((r2 .>. 4) .&. 0x0f0f)  in
     let r4 = r3 `mod` 255                               in
     fromIntegral r4 :: Int64
+  {-# INLINABLE beBitRank #-}
 
 instance BeBitRank Word32 where
   beBitRank v s0 =
@@ -352,6 +426,7 @@ instance BeBitRank Word32 where
     let r3 = (r2 .&. 0x0f0f0f0f) + ((r2 .>. 4) .&. 0x0f0f0f0f)  in
     let r4 = r3 `mod` 255                                       in
     fromIntegral r4 :: Int64
+  {-# INLINABLE beBitRank #-}
 
 instance BeBitRank Word64 where
   beBitRank v s0 =
@@ -364,7 +439,7 @@ instance BeBitRank Word64 where
     let r3 = (r2 .&. 0x0f0f0f0f0f0f0f0f) + ((r2 .>. 4) .&. 0x0f0f0f0f0f0f0f0f)  in
     let r4 = r3 `mod` 255                                                       in
     fromIntegral r4 :: Int64
-
+  {-# INLINABLE beBitRank #-}
 
 instance BeBitSelect Word64 where
   beBitSelect v rn =
@@ -396,27 +471,36 @@ instance BeBitSelect Word64 where
     let s6 = s5 - ((t5 - r5) .&. 256) .>. 8                                     in
     let s7 =      65 - s6                                                       in
     fromIntegral s7
+  {-# INLINABLE beBitSelect #-}
 
 instance BitRank Word8 where
   bitRank = leBitRank
+  {-# INLINABLE bitRank #-}
 
 instance BitRank Word16 where
   bitRank = leBitRank
+  {-# INLINABLE bitRank #-}
 
 instance BitRank Word32 where
   bitRank = leBitRank
+  {-# INLINABLE bitRank #-}
 
 instance BitRank Word64 where
   bitRank = leBitRank
+  {-# INLINABLE bitRank #-}
 
 instance BitSelect Word8 where
   bitSelect = leBitSelect
+  {-# INLINABLE bitSelect #-}
 
 instance BitSelect Word16 where
   bitSelect = leBitSelect
+  {-# INLINABLE bitSelect #-}
 
 instance BitSelect Word32 where
   bitSelect = leBitSelect
+  {-# INLINABLE bitSelect #-}
 
 instance BitSelect Word64 where
   bitSelect = leBitSelect
+  {-# INLINABLE bitSelect #-}
--- src/HaskellWorks/Data/Succinct/Simple.hs
+++ src/HaskellWorks/Data/Succinct/Simple.hs
@@ -34,42 +34,54 @@ newtype SimpleBitVector64 = SimpleBitVector64 (Vector Word64) deriving (Eq, Show
 instance TestBit SimpleBitVector8 where
   SimpleBitVector8  v .?. n = case n `quotRem` bitLength (v ! 0) of
     (q, r) -> (v ! fromIntegral q) .?. fromIntegral r
+  {-# INLINABLE (.?.) #-}
 
 instance TestBit SimpleBitVector16 where
   SimpleBitVector16 v .?. n = case n `quotRem` bitLength (v ! 0) of
     (q, r) -> (v ! fromIntegral q) .?. fromIntegral r
+  {-# INLINABLE (.?.) #-}
 
 instance TestBit SimpleBitVector32 where
   SimpleBitVector32 v .?. n = case n `quotRem` bitLength (v ! 0) of
     (q, r) -> (v ! fromIntegral q) .?. fromIntegral r
+  {-# INLINABLE (.?.) #-}
 
 instance TestBit SimpleBitVector64 where
   SimpleBitVector64 v .?. n = case n `quotRem` bitLength (v ! 0) of
     (q, r) -> (v ! fromIntegral q) .?. fromIntegral r
+  {-# INLINABLE (.?.) #-}
 
 instance BitRank SimpleBitVector8 where
   bitRank (SimpleBitVector8  v) = rankWords (toList v)
+  {-# INLINABLE bitRank #-}
 
 instance BitRank SimpleBitVector16 where
   bitRank (SimpleBitVector16 v) = rankWords (toList v)
+  {-# INLINABLE bitRank #-}
 
 instance BitRank SimpleBitVector32 where
   bitRank (SimpleBitVector32 v) = rankWords (toList v)
+  {-# INLINABLE bitRank #-}
 
 instance BitRank SimpleBitVector64 where
   bitRank (SimpleBitVector64 v) = rankWords (toList v)
+  {-# INLINABLE bitRank #-}
 
 instance BitSelect SimpleBitVector8 where
   bitSelect (SimpleBitVector8  v) = selectWords 0 (toList v)
+  {-# INLINABLE bitSelect #-}
 
 instance BitSelect SimpleBitVector16 where
   bitSelect (SimpleBitVector16 v) = selectWords 0 (toList v)
+  {-# INLINABLE bitSelect #-}
 
 instance BitSelect SimpleBitVector32 where
   bitSelect (SimpleBitVector32 v) = selectWords 0 (toList v)
+  {-# INLINABLE bitSelect #-}
 
 instance BitSelect SimpleBitVector64 where
   bitSelect (SimpleBitVector64 v) = selectWords 0 (toList v)
+  {-# INLINABLE bitSelect #-}
 
 rankWords :: (Num a, PopCount a, BitRank a, BitLength a) => [a] -> Int64 -> Int64
 rankWords ws n = if remainder == 0
@@ -82,6 +94,7 @@ rankWords ws n = if remainder == 0
     predRank = P.sum (P.map (fromIntegral . popCount) ls)
     r = headDef 0 rs
     bitLen = bitLength (P.head ws)
+{-# INLINABLE rankWords #-}
 
 selectWords :: (PopCount v, BitSelect v, BitLength v) => Int64 -> [v] -> Int64 -> Int64
 selectWords _ [] r = r + 1
@@ -90,3 +103,4 @@ selectWords n (w:ws) r = if pc < n
     else bitSelect w n + r
   where
     pc = popCount w
+{-# INLINABLE selectWords #-}
