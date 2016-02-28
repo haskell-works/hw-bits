023f19d4f1b82e5b22097349a51194ddf26c2041
John Ky <newhoggy@gmail.com>
2016-02-28 12:30:41 +1100

    New firstChild, nextSibling, parent, depth and subtreeSize functions.

--- src/HaskellWorks/Data/Succinct/BalancedParens/Internal.hs
+++ src/HaskellWorks/Data/Succinct/BalancedParens/Internal.hs
@@ -1,10 +1,31 @@
 module HaskellWorks.Data.Succinct.BalancedParens.Internal
   ( BalancedParens(..)
+  , firstChild
+  , nextSibling
+  , parent
+  , depth
+  , subtreeSize
   ) where
 
 import HaskellWorks.Data.Positioning
+import HaskellWorks.Data.Succinct.RankSelect.Internal
 
 class BalancedParens v where
   findOpen :: v -> Position -> Position
   findClose :: v -> Position -> Position
   enclose :: v -> Position -> Position
+
+firstChild :: v -> Position -> Position
+firstChild _ = (+ 1)
+
+nextSibling :: (BalancedParens v) => v -> Position -> Position
+nextSibling v p = findClose v p + 1
+
+parent :: (BalancedParens v) => v -> Position -> Position
+parent = enclose
+
+depth :: (BalancedParens v, BitRank v) => v -> Position -> Count
+depth = bitRank
+
+subtreeSize :: (BalancedParens v) => v -> Position -> Position
+subtreeSize v p = (findClose v p - p + 1) `quot` 2
