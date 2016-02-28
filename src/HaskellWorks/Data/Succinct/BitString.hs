f550e8010cd9c8f4ed254756fc47d4fee45e86f8
John Ky <newhoggy@gmail.com>
2016-02-28 11:06:14 +1100

    New toBitString function.

--- src/HaskellWorks/Data/Succinct/BitString.hs
+++ src/HaskellWorks/Data/Succinct/BitString.hs
@@ -1,9 +1,14 @@
 module HaskellWorks.Data.Succinct.BitString
   ( fromBitString
+  , toBitString
   ) where
 
 import           HaskellWorks.Data.Succinct.BitParse
+import           HaskellWorks.Data.Succinct.BitPrint
 import           Text.ParserCombinators.Parsec
 
 fromBitString :: BitParse a => String -> Maybe a
 fromBitString = either (const Nothing) Just . parse bitParse0 "" . filter (/= ' ')
+
+toBitString :: BitPrint a => a -> String
+toBitString a = bitPrint a ""
