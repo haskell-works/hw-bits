f4f5db909d5c77393ae04f732c5bcb4e5d3b3d70
John Ky <newhoggy@gmail.com>
2016-02-28 15:03:55 +1100

    Add Json tokenising support.

---
+++ src/HaskellWorks/Data/Conduit/Json.hs
@@ -0,0 +1,164 @@
+{-# LANGUAGE RankNTypes    #-}
+
+module HaskellWorks.Data.Conduit.Json where
+
+import           Control.Monad
+import           Control.Monad.Trans.Resource             (MonadThrow)
+import           Data.Bits
+import           Data.ByteString                          as BS
+import           Data.ByteString.Internal                 as BS
+import           Data.Conduit
+import           Data.Conduit.List                        as CL
+import           Data.Int
+import           Data.Word
+import           HaskellWorks.Data.Conduit.Tokenize.Attoparsec
+import           HaskellWorks.Data.Conduit.Tokenize.Attoparsec.Offset
+import           HaskellWorks.Data.Json.Final.Tokenize
+import           HaskellWorks.Data.Json.Token
+import           Prelude as P
+import Foreign.Ptr              (plusPtr)
+import Foreign.Storable         (Storable(..))
+
+markerToByteString' :: Monad m => Int64 -> Word8 -> Conduit Int64 m BS.ByteString
+markerToByteString' a v = do
+  mo <- await
+  case mo of
+    Just o -> if o < (a + 8)
+      then markerToByteString' a (bit (fromIntegral (o - a)) .|. v)
+      else do
+        yield $ BS.singleton v
+        leftover o
+        markerToByteString' (a + 8) 0
+    Nothing -> when (v /= 0) $ yield $ BS.singleton v
+
+markerToByteString :: Monad m => Conduit Int64 m BS.ByteString
+markerToByteString = markerToByteString' 0 0
+
+textToJsonToken :: MonadThrow m => Conduit BS.ByteString m (ParseDelta Offset, JsonToken)
+textToJsonToken = conduitParser (Offset 0) parseJsonToken
+
+jsonToken2Markers :: Monad m => Conduit (ParseDelta Offset, JsonToken) m Int64
+jsonToken2Markers = do
+  mi <- await
+  case mi of
+    Just (ParseDelta (Offset start) _, token) -> do
+      case token of
+        JsonTokenBraceL     -> yield $ fromIntegral start
+        JsonTokenBraceR     -> yield $ fromIntegral start
+        JsonTokenBracketL   -> yield $ fromIntegral start
+        JsonTokenBracketR   -> yield $ fromIntegral start
+        JsonTokenComma      -> yield $ fromIntegral start
+        JsonTokenColon      -> yield $ fromIntegral start
+        JsonTokenWhitespace -> return ()
+        JsonTokenString _   -> yield $ fromIntegral start
+        JsonTokenBoolean _  -> yield $ fromIntegral start
+        JsonTokenNumber _   -> yield $ fromIntegral start
+        JsonTokenNull       -> yield $ fromIntegral start
+      jsonToken2Markers
+    Nothing -> return ()
+
+moo :: Source IO (ParseDelta Offset, JsonToken)
+moo = CL.sourceList
+  [ (ParseDelta (Offset 1) (Offset 2), JsonTokenBraceL)
+  ]
+
+------------------------
+
+bitsToByteString :: Monad m => Int64 -> Word8 -> Conduit Bool m BS.ByteString
+bitsToByteString n w = do
+  mb <- await
+  case mb of
+    Just b -> do
+      let v = if b then w .|. bit (fromIntegral (n `mod` 8)) else w
+      when (n `mod` 8 == 7) (yield $ BS.singleton v)
+      bitsToByteString (n + 1) v
+    Nothing -> when (w /= 0) (yield $ BS.singleton w)
+
+yieldBitsOfWord8 :: Monad m => Word8 -> Conduit BS.ByteString m Bool
+yieldBitsOfWord8 w = do
+  yield ((w .&. bit 0) /= 0)
+  yield ((w .&. bit 1) /= 0)
+  yield ((w .&. bit 2) /= 0)
+  yield ((w .&. bit 3) /= 0)
+  yield ((w .&. bit 4) /= 0)
+  yield ((w .&. bit 5) /= 0)
+  yield ((w .&. bit 6) /= 0)
+  yield ((w .&. bit 7) /= 0)
+
+yieldBitsofWord8s :: Monad m => [Word8] -> Conduit BS.ByteString m Bool
+yieldBitsofWord8s = P.foldr ((>>) . yieldBitsOfWord8) (return ())
+
+byteStringToBits :: Monad m => Conduit BS.ByteString m Bool
+byteStringToBits = do
+  mbs <- await
+  case mbs of
+    Just bs -> yieldBitsofWord8s (BS.unpack bs) >> byteStringToBits
+    Nothing -> return ()
+
+runListConduit :: [i] -> Conduit i [] o -> [o]
+runListConduit is c = P.concat $ sourceList is =$ c $$ consume
+
+mcons :: Maybe a -> [a] -> [a]
+mcons (Just a) = (a:)
+mcons Nothing = id
+
+awaitWord8Or :: Monad m => Word8 -> Consumer ByteString m Word8
+awaitWord8Or d = do
+  mbs <- await
+  case mbs of
+    Just bs -> if BS.null bs
+      then do
+        leftover $ BS.tail bs
+        return   $ BS.head bs
+      else return d
+    Nothing -> return d
+
+unfinished :: Monad m => Consumer i m Bool
+unfinished = do
+  mi <- await
+  case mi of
+    Just i  -> leftover i >> return True
+    Nothing -> return False
+
+ifM :: Monad m => m Bool -> m a -> m a -> m a
+ifM p a b = do
+  t <- p
+  if t then a else b
+
+whenM :: Monad m => m Bool -> m () -> m ()
+whenM p a = ifM p a (return ())
+
+packW64 :: Word64 -> ByteString
+packW64 w64 =
+    unsafeCreate 8 $ \p -> do
+      poke  p               (fromIntegral ( w64                 .|. 0xff) :: Word8)
+      poke (p `plusPtr` 1)  (fromIntegral ((w64 `shift` (-8 ))  .|. 0xff) :: Word8)
+      poke (p `plusPtr` 2)  (fromIntegral ((w64 `shift` (-16))  .|. 0xff) :: Word8)
+      poke (p `plusPtr` 3)  (fromIntegral ((w64 `shift` (-24))  .|. 0xff) :: Word8)
+      poke (p `plusPtr` 4)  (fromIntegral ((w64 `shift` (-32))  .|. 0xff) :: Word8)
+      poke (p `plusPtr` 5)  (fromIntegral ((w64 `shift` (-40))  .|. 0xff) :: Word8)
+      poke (p `plusPtr` 6)  (fromIntegral ((w64 `shift` (-48))  .|. 0xff) :: Word8)
+      poke (p `plusPtr` 7)  (fromIntegral ((w64 `shift` (-56))  .|. 0xff) :: Word8)
+
+bsToRank9BS :: Monad m => Word64 -> Conduit BS.ByteString m BS.ByteString
+bsToRank9BS rank = whenM unfinished $ do
+  v0 <- awaitWord8Or 0
+  v1 <- awaitWord8Or 0
+  v2 <- awaitWord8Or 0
+  v3 <- awaitWord8Or 0
+  v4 <- awaitWord8Or 0
+  v5 <- awaitWord8Or 0
+  v6 <- awaitWord8Or 0
+  v7 <- awaitWord8Or 0
+  let p0 =       fromIntegral (popCount v0)             :: Word64
+  let p1 = p0 + (fromIntegral (popCount v1) `shift` 9 ) :: Word64
+  let p2 = p1 + (fromIntegral (popCount v2) `shift` 18) :: Word64
+  let p3 = p2 + (fromIntegral (popCount v3) `shift` 27) :: Word64
+  let p4 = p3 + (fromIntegral (popCount v4) `shift` 36) :: Word64
+  let p5 = p4 + (fromIntegral (popCount v5) `shift` 45) :: Word64
+  let p6 = p5 + (fromIntegral (popCount v6) `shift` 54) :: Word64
+  let p7 = p6 + (fromIntegral (popCount v7) `shift` 54) :: Word64
+  let newRank = rank + p7
+  yield $ packW64 (p0 .|. p1 .|. p2 .|. p3 .|. p4 .|. p5 .|. p6)
+  yield $ packW64 newRank
+  bsToRank9BS newRank
---
+++ test/HaskellWorks/Data/Conduit/JsonSpec.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE OverloadedStrings    #-}
+
+module HaskellWorks.Data.Conduit.JsonSpec (spec) where
+
+import           Control.Monad.Trans.Resource (MonadThrow)
+import           Data.ByteString
+import           Data.Conduit
+import           Data.Int
+import           HaskellWorks.Data.Bits.Conversion
+import           HaskellWorks.Data.Conduit.Json
+import           HaskellWorks.Data.Conduit.Tokenize.Attoparsec
+import           HaskellWorks.Data.Conduit.Tokenize.Attoparsec.Offset
+import           HaskellWorks.Data.Json.Token
+import           Test.Hspec
+
+markerToBits :: [Int64] -> [Bool]
+markerToBits ms = runListConduit ms (markerToByteString =$= byteStringToBits)
+
+jsonToBits :: [ByteString] -> [Bool]
+jsonToBits json = runListConduit json $
+  textToJsonToken =$= jsonToken2Markers =$= markerToByteString =$= byteStringToBits
+
+jsonToMarkerBS :: [ByteString] -> [ByteString]
+jsonToMarkerBS json = runListConduit json $
+  textToJsonToken =$= jsonToken2Markers =$= markerToByteString
+
+jsonToMarkers :: [ByteString] -> [Int64]
+jsonToMarkers json = runListConduit json $
+  textToJsonToken =$= jsonToken2Markers
+
+jsonToken2Markers2 :: [(ParseDelta Offset, JsonToken)] -> [Int64]
+jsonToken2Markers2 json = runListConduit json $ jsonToken2Markers
+
+identity :: Monad m => Conduit i m i
+identity = do
+  ma <- await
+  case ma of
+    Just a -> yield a
+    Nothing -> return ()
+
+spec :: Spec
+spec = describe "Data.Conduit.Succinct.JsonSpec" $ do
+  it "No markers should produce no bits" $
+    markerToBits [] `shouldBe` []
+  it "One marker < 8 should produce one byte with one bit set" $ do
+    markerToBits [0] `shouldBe` stringToBits "10000000"
+    markerToBits [1] `shouldBe` stringToBits "01000000"
+    markerToBits [2] `shouldBe` stringToBits "00100000"
+    markerToBits [3] `shouldBe` stringToBits "00010000"
+    markerToBits [4] `shouldBe` stringToBits "00001000"
+    markerToBits [5] `shouldBe` stringToBits "00000100"
+    markerToBits [6] `shouldBe` stringToBits "00000010"
+    markerToBits [7] `shouldBe` stringToBits "00000001"
+  it "One 8 <= marker < 16 should produce one byte empty byte and another byte with one bit set" $ do
+    markerToBits [ 8] `shouldBe` stringToBits "00000000 10000000"
+    markerToBits [ 9] `shouldBe` stringToBits "00000000 01000000"
+    markerToBits [10] `shouldBe` stringToBits "00000000 00100000"
+    markerToBits [11] `shouldBe` stringToBits "00000000 00010000"
+    markerToBits [12] `shouldBe` stringToBits "00000000 00001000"
+    markerToBits [13] `shouldBe` stringToBits "00000000 00000100"
+    markerToBits [14] `shouldBe` stringToBits "00000000 00000010"
+    markerToBits [15] `shouldBe` stringToBits "00000000 00000001"
+  it "All markers 0 .. 7 should produce one full byte" $
+    markerToBits [0, 1, 2, 3, 4, 5, 6, 7] `shouldBe` stringToBits "11111111"
+  it "All markers 0 .. 7 except 1 should produce one almost full byte" $ do
+    markerToBits [1, 2, 3, 4, 5, 6, 7] `shouldBe` stringToBits "01111111"
+    markerToBits [0, 2, 3, 4, 5, 6, 7] `shouldBe` stringToBits "10111111"
+    markerToBits [0, 1, 3, 4, 5, 6, 7] `shouldBe` stringToBits "11011111"
+    markerToBits [0, 1, 2, 4, 5, 6, 7] `shouldBe` stringToBits "11101111"
+    markerToBits [0, 1, 2, 3, 5, 6, 7] `shouldBe` stringToBits "11110111"
+    markerToBits [0, 1, 2, 3, 4, 6, 7] `shouldBe` stringToBits "11111011"
+    markerToBits [0, 1, 2, 3, 4, 5, 7] `shouldBe` stringToBits "11111101"
+    markerToBits [0, 1, 2, 3, 4, 5, 6] `shouldBe` stringToBits "11111110"
+  it "All markers 0 .. 8 should produce one almost full byte and one near empty byte" $
+    markerToBits [0, 1, 2, 3, 4, 5, 6, 7, 8] `shouldBe` stringToBits "11111111 10000000"
+  it "Matching bits for bytes" $
+    runListConduit [pack [0x80], pack [0xff], pack [0x01]] byteStringToBits `shouldBe`
+      stringToBits "00000001 11111111 10000000"
+  it "Every interesting token should produce a marker" $
+    jsonToken2Markers2 [
+      (ParseDelta (Offset 0) (Offset 1), JsonTokenBraceL),
+      (ParseDelta (Offset 1) (Offset 2), JsonTokenBraceL),
+      (ParseDelta (Offset 2) (Offset 3), JsonTokenBraceR),
+      (ParseDelta (Offset 3) (Offset 4), JsonTokenBraceR)] `shouldBe` [0, 1, 2, 3]
+  describe "When converting Json to tokens to markers to bits" $ do
+    it "Empty Json should produce no bits" $
+      jsonToBits [""] `shouldBe` []
+    it "Spaces and newlines should produce no bits" $
+      jsonToBits ["  \n \r \t "] `shouldBe` []
+    it "number at beginning should produce one bit" $
+      jsonToBits ["1234 "] `shouldBe` stringToBits "10000000"
+    it "false at beginning should produce one bit" $
+      jsonToBits ["false "] `shouldBe` stringToBits "10000000"
+    it "true at beginning should produce one bit" $
+      jsonToBits ["true "] `shouldBe` stringToBits "10000000"
+    it "string at beginning should produce one bit" $
+      jsonToBits ["\"hello\" "] `shouldBe` stringToBits "10000000"
+    it "left brace at beginning should produce one bit" $
+      jsonToBits ["{ "] `shouldBe` stringToBits "10000000"
+    it "right brace at beginning should produce one bit" $
+      jsonToBits ["} "] `shouldBe` stringToBits "10000000"
+    it "left bracket at beginning should produce one bit" $
+      jsonToBits ["[ "] `shouldBe` stringToBits "10000000"
+    it "right bracket at beginning should produce one bit" $
+      jsonToBits ["] "] `shouldBe` stringToBits "10000000"
+    it "right bracket at beginning should produce one bit" $
+      jsonToBits [": "] `shouldBe` stringToBits "10000000"
+    it "right bracket at beginning should produce one bit" $
+      jsonToBits [", "] `shouldBe` stringToBits "10000000"
+    it "Four consecutive braces should produce four bits" $
+      jsonToBits ["{{}}"] `shouldBe` stringToBits "11110000"
+    it "Four spread out braces should produce four spread out bits" $
+      jsonToBits [" { { } } "] `shouldBe` stringToBits "01010101"
