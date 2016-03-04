a7c68cc82d0668468d3f9d264695405f4d693679
John Ky <newhoggy@gmail.com>
2016-03-05 10:21:55 +1100

    Fix tests.

--- test/HaskellWorks/Data/Succinct/BalancedParensSpec.hs
+++ test/HaskellWorks/Data/Succinct/BalancedParensSpec.hs
@@ -8,16 +8,19 @@ import           HaskellWorks.Data.Bits.BitString
 import           HaskellWorks.Data.Succinct.BalancedParens
 import           Test.Hspec
 
+{-# ANN module ("HLint: ignore Redundant do"        :: String) #-}
+{-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
+
 spec :: Spec
 spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
-  -- describe "For (()(()())) 0010010111" $ do
-  --   let bs = SimpleBalancedParens (0x3A4 :: Word64)
-  --   it "Test 1a" $ findClose bs 0 `shouldBe` 9
-  --   it "Test 1b" $ findClose bs 1 `shouldBe` 2
-  --   it "Test 2a" $ findOpen  bs 9 `shouldBe` 0
-  --   it "Test 2b" $ findOpen  bs 2 `shouldBe` 1
-  --   it "Test 3a" $ enclose   bs 1 `shouldBe` 0
-  --   it "Test 3b" $ enclose   bs 6 `shouldBe` 3
+  describe "For (()(()())) 0010010111" $ do
+    let bs = SimpleBalancedParens (91 :: Word64)
+    it "Test 1a" $ findClose bs 0 `shouldBe` 9
+    it "Test 1b" $ findClose bs 1 `shouldBe` 2
+    it "Test 2a" $ findOpen  bs 9 `shouldBe` 0
+    it "Test 2b" $ findOpen  bs 2 `shouldBe` 1
+    it "Test 3a" $ enclose   bs 1 `shouldBe` 0
+    it "Test 3b" $ enclose   bs 6 `shouldBe` 3
   describe "For (()(()())) 1101101000" $ do
     let bs = SimpleBalancedParens (fromJust (fromBitString "1101101000") :: [Bool])
     it "Test 1a" $ findClose bs 0 `shouldBe` 9
@@ -26,3 +29,4 @@ spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
     it "Test 2b" $ findOpen  bs 2 `shouldBe` 1
     it "Test 3a" $ enclose   bs 1 `shouldBe` 0
     it "Test 3b" $ enclose   bs 6 `shouldBe` 3
+
