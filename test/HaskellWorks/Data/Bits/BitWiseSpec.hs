54db14ea6624377c447162074f85c976a1dd2670
John Ky <newhoggy@gmail.com>
2016-03-09 17:51:44 +1100

    Move rank select tests to proper place.

--- test/HaskellWorks/Data/Bits/BitWiseSpec.hs
+++ test/HaskellWorks/Data/Bits/BitWiseSpec.hs
@@ -3,41 +3,14 @@
 
 module HaskellWorks.Data.Bits.BitWiseSpec (spec) where
 
-import qualified Data.Bits                             as B
-import           Data.Int
+import qualified Data.Bits                      as B
 import           Data.Word
 import           HaskellWorks.Data.Bits.BitWise
 import           HaskellWorks.Data.Positioning
-import           HaskellWorks.Data.Succinct.RankSelect
 import           Test.Hspec
 import           Test.QuickCheck
 
 {-# ANN module ("HLint: ignore Redundant do" :: String) #-}
-
-newtype Count_0_8  = Count_0_8  Count deriving (Eq, Show)
-newtype Count_0_16 = Count_0_16 Count deriving (Eq, Show)
-newtype Count_0_32 = Count_0_32 Count deriving (Eq, Show)
-newtype Count_0_64 = Count_0_64 Count deriving (Eq, Show)
-
-instance Arbitrary Count_0_8 where
-  arbitrary = do
-     n <- choose (0, 8 :: Word64)
-     return (Count_0_8 (Count n))
-
-instance Arbitrary Count_0_16 where
- arbitrary = do
-    n <- choose (0, 16 :: Word64)
-    return (Count_0_16 (Count n))
-
-instance Arbitrary Count_0_32 where
- arbitrary = do
-    n <- choose (0, 32 :: Word64)
-    return (Count_0_32 (Count n))
-
-instance Arbitrary Count_0_64 where
- arbitrary = do
-    n <- choose (0, 64 :: Word64)
-    return (Count_0_64 (Count n))
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
@@ -49,17 +22,3 @@ spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
     \(w :: Word32) -> popCount1 w == fromIntegral (B.popCount w)
   it "popCount1 for Word64 matches Data.Bits implementation" $ property $
     \(w :: Word64) -> popCount1 w == fromIntegral (B.popCount w)
-  it "rank1 for Word16 and Word64 should give same answer for bits 0-7" $ property $
-    \(Count_0_8  i) (w :: Word8 ) -> rank1 w i == rank1 (fromIntegral w :: Word64) i
-  it "rank1 for Word16 and Word64 should give same answer for bits 0-15" $ property $
-    \(Count_0_16 i) (w :: Word16) -> rank1 w i == rank1 (fromIntegral w :: Word64) i
-  it "rank1 for Word32 and Word64 should give same answer for bits 0-31" $ property $
-    \(Count_0_32 i) (w :: Word32) -> rank1 w i == rank1 (fromIntegral w :: Word64) i
-  it "rank1 for Word32 and Word64 should give same answer for bits 32-64" $ property $
-    \(Count_0_32 i) (v :: Word32) (w :: Word32) ->
-      let v64 = fromIntegral v :: Word64 in
-      let w64 = fromIntegral w :: Word64 in
-      rank1 v i + popCount1 w == rank1 ((v64 .<. 32) .|. w64) (i + 32)
-  it "rank1 and select1 for Word64 form a galois connection" $ property $
-    \(Count_0_32 i) (w :: Word32) -> 1 <= i && i <= popCount1 w ==>
-      rank1 w (select1 w i) == i && select1 w (rank1 w (fromIntegral i)) <= (fromIntegral i)
--- test/HaskellWorks/Data/Succinct/RankSelect/InternalSpec.hs
+++ test/HaskellWorks/Data/Succinct/RankSelect/InternalSpec.hs
@@ -16,6 +16,31 @@ import           Test.QuickCheck
 {-# ANN module ("HLint: ignore Redundant do" :: String) #-}
 {-# ANN module ("HLint: ignore Reduce duplication"  :: String) #-}
 
+newtype Count_0_8  = Count_0_8  Count deriving (Eq, Show)
+newtype Count_0_16 = Count_0_16 Count deriving (Eq, Show)
+newtype Count_0_32 = Count_0_32 Count deriving (Eq, Show)
+newtype Count_0_64 = Count_0_64 Count deriving (Eq, Show)
+
+instance Arbitrary Count_0_8 where
+  arbitrary = do
+     n <- choose (0, 8 :: Word64)
+     return (Count_0_8 (Count n))
+
+instance Arbitrary Count_0_16 where
+ arbitrary = do
+    n <- choose (0, 16 :: Word64)
+    return (Count_0_16 (Count n))
+
+instance Arbitrary Count_0_32 where
+ arbitrary = do
+    n <- choose (0, 32 :: Word64)
+    return (Count_0_32 (Count n))
+
+instance Arbitrary Count_0_64 where
+ arbitrary = do
+    n <- choose (0, 64 :: Word64)
+    return (Count_0_64 (Count n))
+
 spec :: Spec
 spec = describe "HaskellWorks.Data.Succinct.RankSelect.InternalSpec" $ do
   describe "For [Bool]" $ do
@@ -54,6 +79,21 @@ spec = describe "HaskellWorks.Data.Succinct.RankSelect.InternalSpec" $ do
     it "select False 10010010 over [0..5] should be 023568" $ do
       let (Just bs) = fromBitString "10010010" :: Maybe Word8
       fmap (select0 bs) [0..5] `shouldBe` [0, 2, 3, 5, 6, 8]
+  describe "For Word64" $ do
+    it "rank1 for Word16 and Word64 should give same answer for bits 0-7" $ property $
+      \(Count_0_8  i) (w :: Word8 ) -> rank1 w i == rank1 (fromIntegral w :: Word64) i
+    it "rank1 for Word16 and Word64 should give same answer for bits 0-15" $ property $
+      \(Count_0_16 i) (w :: Word16) -> rank1 w i == rank1 (fromIntegral w :: Word64) i
+    it "rank1 for Word32 and Word64 should give same answer for bits 0-31" $ property $
+      \(Count_0_32 i) (w :: Word32) -> rank1 w i == rank1 (fromIntegral w :: Word64) i
+    it "rank1 for Word32 and Word64 should give same answer for bits 32-64" $ property $
+      \(Count_0_32 i) (v :: Word32) (w :: Word32) ->
+        let v64 = fromIntegral v :: Word64 in
+        let w64 = fromIntegral w :: Word64 in
+        rank1 v i + popCount1 w == rank1 ((v64 .<. 32) .|. w64) (i + 32)
+    it "rank1 and select1 for Word64 form a galois connection" $ property $
+      \(Count_0_32 i) (w :: Word32) -> 1 <= i && i <= popCount1 w ==>
+        rank1 w (select1 w i) == i && select1 w (rank1 w (fromIntegral i)) <= (fromIntegral i)
   describe "For (DVS.Vector Word8)" $ do
     it "rank True 10010010 over [0..8] should be 011122233" $ do
       let (Just bs) = fromBitString "10010010" :: Maybe (DVS.Vector Word8)
