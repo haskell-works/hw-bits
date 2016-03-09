8d015b35484a595096704dc57f5d078af8cde52c
John Ky <newhoggy@gmail.com>
2016-03-09 19:14:36 +1100

    More BitWise tests.

--- test/HaskellWorks/Data/Bits/BitWiseSpec.hs
+++ test/HaskellWorks/Data/Bits/BitWiseSpec.hs
@@ -80,3 +80,11 @@ spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
       \(w :: Word32) -> popCount1 w == fromIntegral (B.popCount w)
     it "for Word64 matches Data.Bits implementation" $ property $
       \(w :: Word64) -> popCount1 w == fromIntegral (B.popCount w)
+    it "for [Word8] matches Data.Bits implementation" $ property $
+      \(w :: [Word8] ) -> popCount1 w == sum (fmap (fromIntegral . B.popCount) w)
+    it "for [Word16] matches Data.Bits implementation" $ property $
+      \(w :: [Word16]) -> popCount1 w == sum (fmap (fromIntegral . B.popCount) w)
+    it "for [Word32] matches Data.Bits implementation" $ property $
+      \(w :: [Word32]) -> popCount1 w == sum (fmap (fromIntegral . B.popCount) w)
+    it "for [Word64] matches Data.Bits implementation" $ property $
+      \(w :: [Word64]) -> popCount1 w == sum (fmap (fromIntegral . B.popCount) w)
