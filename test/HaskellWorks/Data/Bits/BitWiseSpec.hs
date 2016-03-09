38b17ac09891ab766b2afa6f0fd0547a52b8db45
John Ky <newhoggy@gmail.com>
2016-03-09 19:10:30 +1100

    More BitWise tests.

--- test/HaskellWorks/Data/Bits/BitWiseSpec.hs
+++ test/HaskellWorks/Data/Bits/BitWiseSpec.hs
@@ -12,13 +12,63 @@ import           Test.QuickCheck
 
 {-# ANN module ("HLint: ignore Redundant do" :: String) #-}
 
+-- class BitLength v where
+--   bitLength :: v -> Count
+
+--   endPosition :: v -> Position
+--   endPosition = Position . fromIntegral . getCount . bitLength
+
+-- class Shift a where
+--   (.<.) :: a -> Count -> a
+--   (.>.) :: a -> Count -> a
+
+-- class TestBit a where
+--   (.?.) :: a -> Position -> Bool
+
+-- class BitWise a where
+--   (.&.) :: a -> a -> a
+--   (.|.) :: a -> a -> a
+--   (.^.) :: a -> a -> a
+--   comp  :: a -> a
+--   all0s :: a
+--   all1s :: a
+
+-- class PopCount v e where
+--   popCount :: e -> v -> Count
+
+-- class PopCount0 v where
+--   popCount0 :: v -> Count
+
+-- class PopCount1 v where
+--   popCount1 :: v -> Count
+
+-- --------------------------------------------------------------------------------
+-- -- Functions
+
+-- elemBitLength :: (VectorLike v e, BitLength e) => v e -> Count
+-- elemBitLength v = bitLength (v !!! 0)
+
+-- elemBitEnd :: (VectorLike v e, BitLength e) => v e -> Position
+-- elemBitEnd v = endPosition (v !!! 0)
+
+
 spec :: Spec
 spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
-  it "popCount1 for Word8 matches Data.Bits implementation" $ property $
-    \(w :: Word8 ) -> popCount1 w == fromIntegral (B.popCount w)
-  it "popCount1 for Word16 matches Data.Bits implementation" $ property $
-    \(w :: Word16) -> popCount1 w == fromIntegral (B.popCount w)
-  it "popCount1 for Word32 matches Data.Bits implementation" $ property $
-    \(w :: Word32) -> popCount1 w == fromIntegral (B.popCount w)
-  it "popCount1 for Word64 matches Data.Bits implementation" $ property $
-    \(w :: Word64) -> popCount1 w == fromIntegral (B.popCount w)
+  describe "for popCount0" $ do
+    it "for Word8 matches Data.Bits implementation" $ property $
+      \(w :: Word8 ) -> popCount0 w == bitLength w - fromIntegral (B.popCount w)
+    it "for Word16 matches Data.Bits implementation" $ property $
+      \(w :: Word16) -> popCount0 w == bitLength w - fromIntegral (B.popCount w)
+    it "for Word32 matches Data.Bits implementation" $ property $
+      \(w :: Word32) -> popCount0 w == bitLength w - fromIntegral (B.popCount w)
+    it "for Word64 matches Data.Bits implementation" $ property $
+      \(w :: Word64) -> popCount0 w == bitLength w - fromIntegral (B.popCount w)
+  describe "for popCount1" $ do
+    it "for Word8 matches Data.Bits implementation" $ property $
+      \(w :: Word8 ) -> popCount1 w == fromIntegral (B.popCount w)
+    it "for Word16 matches Data.Bits implementation" $ property $
+      \(w :: Word16) -> popCount1 w == fromIntegral (B.popCount w)
+    it "for Word32 matches Data.Bits implementation" $ property $
+      \(w :: Word32) -> popCount1 w == fromIntegral (B.popCount w)
+    it "for Word64 matches Data.Bits implementation" $ property $
+      \(w :: Word64) -> popCount1 w == fromIntegral (B.popCount w)
