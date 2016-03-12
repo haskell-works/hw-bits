596150d02559e6173cb4de9a8bec3d01e6bb779b
John Ky <newhoggy@gmail.com>
2016-03-12 23:44:40 +1100

    Implementation of FromBools Word32 and FromBools Word64 with tests.

--- src/HaskellWorks/Data/Bits/FromBools.hs
+++ src/HaskellWorks/Data/Bits/FromBools.hs
@@ -34,3 +34,23 @@ instance FromBools Word16 where
                                     go (n + 1) (if x then w .|. (1 .<. n) else w) xs
             | n < 0               = error "Invalid index"
             | otherwise           = Just (w, x:xs)
+
+instance FromBools Word32 where
+  fromBools [] = Nothing
+  fromBools xs = go 0 0 xs
+    where go _ w []        = Just (w, [])
+          go n w (x:xs)
+            | n < fixedBitSize w  = trace ("n = " ++ show n) $
+                                    go (n + 1) (if x then w .|. (1 .<. n) else w) xs
+            | n < 0               = error "Invalid index"
+            | otherwise           = Just (w, x:xs)
+
+instance FromBools Word64 where
+  fromBools [] = Nothing
+  fromBools xs = go 0 0 xs
+    where go _ w []        = Just (w, [])
+          go n w (x:xs)
+            | n < fixedBitSize w  = trace ("n = " ++ show n) $
+                                    go (n + 1) (if x then w .|. (1 .<. n) else w) xs
+            | n < 0               = error "Invalid index"
+            | otherwise           = Just (w, x:xs)
--- test/HaskellWorks/Data/Bits/FromBoolsSpec.hs
+++ test/HaskellWorks/Data/Bits/FromBoolsSpec.hs
@@ -45,3 +45,113 @@ spec = describe "HaskellWorks.Data.Bits.FromBoolsSpec" $ do
                               , True, False, True, False
                               ] :: Maybe (Word16, [Bool])
       result `shouldBe` Just (0x555, [])
+    it "fromBool works for 16 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              ] :: Maybe (Word16, [Bool])
+      result `shouldBe` Just (0x5555, [])
+    it "fromBool works for 20 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False
+                              ] :: Maybe (Word16, [Bool])
+      result `shouldBe` Just (0x5555, [True, False, True, False])
+  describe "for Word32" $ do
+    it "fromBool works for 0 bits" $ do
+      let result = fromBools [] :: Maybe (Word32, [Bool])
+      result `shouldBe` Nothing
+    it "fromBool works for 4 bits" $ do
+      let result = fromBools  [ True, False, True, False
+                              ] :: Maybe (Word32, [Bool])
+      result `shouldBe` Just (0x5, [])
+    it "fromBool works for 8 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              ] :: Maybe (Word32, [Bool])
+      result `shouldBe` Just (0x55, [])
+    it "fromBool works for 12 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False
+                              ] :: Maybe (Word32, [Bool])
+      result `shouldBe` Just (0x555, [])
+    it "fromBool works for 20 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False
+                              ] :: Maybe (Word32, [Bool])
+      result `shouldBe` Just (0x55555, [])
+    it "fromBool works for 32 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              ] :: Maybe (Word32, [Bool])
+      result `shouldBe` Just (0x55555555, [])
+    it "fromBool works for 36 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False
+                              ] :: Maybe (Word32, [Bool])
+      result `shouldBe` Just (0x55555555, [True, False, True, False])
+  describe "for Word64" $ do
+    it "fromBool works for 0 bits" $ do
+      let result = fromBools [] :: Maybe (Word64, [Bool])
+      result `shouldBe` Nothing
+    it "fromBool works for 4 bits" $ do
+      let result = fromBools  [ True, False, True, False
+                              ] :: Maybe (Word64, [Bool])
+      result `shouldBe` Just (0x5, [])
+    it "fromBool works for 8 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              ] :: Maybe (Word64, [Bool])
+      result `shouldBe` Just (0x55, [])
+    it "fromBool works for 12 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False
+                              ] :: Maybe (Word64, [Bool])
+      result `shouldBe` Just (0x555, [])
+    it "fromBool works for 20 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False
+                              ] :: Maybe (Word64, [Bool])
+      result `shouldBe` Just (0x55555, [])
+    it "fromBool works for 32 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              ] :: Maybe (Word64, [Bool])
+      result `shouldBe` Just (0x55555555, [])
+    it "fromBool works for 36 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False
+                              ] :: Maybe (Word64, [Bool])
+      result `shouldBe` Just (0x555555555, [])
+    it "fromBool works for 64 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              ] :: Maybe (Word64, [Bool])
+      result `shouldBe` Just (0x5555555555555555, [])
+    it "fromBool works for 68 bits" $ do
+      let result = fromBools  [ True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False, True, False, True, False
+                              , True, False, True, False
+                              ] :: Maybe (Word64, [Bool])
+      result `shouldBe` Just (0x5555555555555555, [True, False, True, False])
