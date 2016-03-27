593fd8ac0595dd13792aa8b025f796b143619d66
John Ky <newhoggy@gmail.com>
2016-03-27 23:38:26 +1100

    Move HasJsonCursorType into sub-module.

--- src/HaskellWorks/Data/Json/Succinct/Cursor.hs
+++ src/HaskellWorks/Data/Json/Succinct/Cursor.hs
@@ -8,7 +8,6 @@ module HaskellWorks.Data.Json.Succinct.Cursor where
 import qualified Data.Attoparsec.ByteString.Char8                           as ABC
 import qualified Data.ByteString                                            as BS
 import           Data.ByteString.Internal                                   as BSI
-import           Data.Char
 import qualified Data.Vector.Storable                                       as DVS
 import           Data.Word
 import           Foreign.ForeignPtr
@@ -24,53 +23,14 @@ import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank1
 import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Select1
 import           HaskellWorks.Data.Vector.VectorLike
 
-class HasJsonCursorType k where
-  jsonCursorType :: k -> JsonCursorType
-
 class FromForeignRegion a where
   fromForeignRegion :: (ForeignPtr Word8, Int, Int) -> a
-
-jsonCursorType' :: Char -> JsonCursorType
-jsonCursorType' c = case c of
-  '[' -> JsonCursorArray
-  't' -> JsonCursorBool
-  'f' -> JsonCursorBool
-  '0' -> JsonCursorNumber
-  '1' -> JsonCursorNumber
-  '2' -> JsonCursorNumber
-  '3' -> JsonCursorNumber
-  '4' -> JsonCursorNumber
-  '5' -> JsonCursorNumber
-  '6' -> JsonCursorNumber
-  '7' -> JsonCursorNumber
-  '8' -> JsonCursorNumber
-  '9' -> JsonCursorNumber
-  '+' -> JsonCursorNumber
-  '-' -> JsonCursorNumber
-  'n' -> JsonCursorNull
-  '{' -> JsonCursorObject
-  '"' -> JsonCursorString
-  _   -> error "Invalid JsonCursor cursorRank"
-
-jsonCursorPos :: (Rank1 w, Select1 v, VectorLike s) => JsonCursor s v w -> Position
-jsonCursorPos k = toPosition (select1 ik (rank1 bpk (cursorRank k)) - 1)
-  where ik  = interests k
-        bpk = balancedParens k
-
-jsonCursorElemAt :: (Rank1 w, Select1 v, VectorLike s) => JsonCursor s v w -> Elem s
-jsonCursorElemAt k = cursorText k !!! jsonCursorPos k
 
 jsonTokenAt :: (Rank1 w, Select1 v) => JsonCursor ByteString v w -> JsonToken
 jsonTokenAt k = case ABC.parse parseJsonToken (vDrop (toCount (jsonCursorPos k)) (cursorText k)) of
   ABC.Fail    {}  -> error "Failed to parse token in cursor"
   ABC.Partial _   -> error "Failed to parse token in cursor"
   ABC.Done    _ r -> r
-
-instance (Rank1 i, Select1 i, Rank1 b) => HasJsonCursorType (JsonCursor String i b) where
-  jsonCursorType = jsonCursorType' . jsonCursorElemAt
-
-instance (Rank1 i, Select1 i, Rank1 b) => HasJsonCursorType (JsonCursor BS.ByteString i b) where
-  jsonCursorType = jsonCursorType' . chr . fromIntegral . jsonCursorElemAt
 
 firstChild :: JsonCursor t v u -> JsonCursor t v u
 firstChild k = k { cursorRank = BP.firstChild (balancedParens k) (cursorRank k) }
