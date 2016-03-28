3cc15df4fd75b78ac7c284e35c755286d97d9c25
John Ky <newhoggy@gmail.com>
2016-03-28 18:26:20 +1100

    Avoid use of Arbitrary where possible.

--- test/HaskellWorks/Data/Succinct/SimpleSpec.hs
+++ test/HaskellWorks/Data/Succinct/SimpleSpec.hs
@@ -5,9 +5,9 @@ module HaskellWorks.Data.Succinct.SimpleSpec (spec) where
 
 import           Data.Vector
 import           Data.Word
-import           HaskellWorks.Data.Arbitrary.Count
 import           HaskellWorks.Data.Bits.BitShown
 import           HaskellWorks.Data.Bits.BitWise
+import           HaskellWorks.Data.Positioning
 import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank1
 import           Test.Hspec
 import           Test.QuickCheck
@@ -16,9 +16,9 @@ import           Test.QuickCheck
 
 spec :: Spec
 spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
-  it "rank1 for BitShown (Vector Word8) and BitShown (Vector Word64) should give same answer" $ property $
-    \(Count_0_64 i) (a :: Word8) (b :: Word8) (c :: Word8) (d :: Word8)
-                  (e :: Word8) (f :: Word8) (g :: Word8) (h :: Word8) ->
+  it "rank1 for BitShown (Vector Word8) and BitShown (Vector Word64) should give same answer" $
+    forAll (choose (0, 64)) $ \(i :: Count) (a :: Word8) (b :: Word8) (c :: Word8) (d :: Word8)
+                                            (e :: Word8) (f :: Word8) (g :: Word8) (h :: Word8) ->
       let a64 = fromIntegral a :: Word64 in
       let b64 = fromIntegral b :: Word64 in
       let c64 = fromIntegral c :: Word64 in
@@ -29,11 +29,11 @@ spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
       let h64 = fromIntegral h :: Word64 in
       let abcdefgh64 = (h64 .<. 56) .|. (g64 .<. 48) .|. (f64 .<. 40) .|. (e64 .<. 32) .|.
                        (d64 .<. 24) .|. (c64 .<. 16) .|. (b64 .<. 8 ) .|.  a64              in
-      let vec16 = BitShown (fromList [a, b, c, d, e, f, g, h] :: Vector Word8 )               in
-      let vec64 = BitShown (fromList [abcdefgh64]             :: Vector Word64)               in
+      let vec16 = BitShown (fromList [a, b, c, d, e, f, g, h] :: Vector Word8 )             in
+      let vec64 = BitShown (fromList [abcdefgh64]             :: Vector Word64)             in
       rank1 vec16 i == rank1 vec64 i
-  it "rank1 for BitShown (Vector Word16) and BitShown (Vector Word64) should give same answer" $ property $
-    \(Count_0_64 i) (a :: Word16) (b :: Word16) (c :: Word16) (d :: Word16) ->
+  it "rank1 for BitShown (Vector Word16) and BitShown (Vector Word64) should give same answer" $
+    forAll (choose (0, 64)) $ \(i :: Count) (a :: Word16) (b :: Word16) (c :: Word16) (d :: Word16) ->
       let a64 = fromIntegral a :: Word64 in
       let b64 = fromIntegral b :: Word64 in
       let c64 = fromIntegral c :: Word64 in
@@ -42,8 +42,8 @@ spec = describe "HaskellWorks.Data.SuccinctSpec" $ do
       let vec16 = BitShown (fromList [a, b, c, d] :: Vector Word16) in
       let vec64 = BitShown (fromList [abcd64]     :: Vector Word64) in
       rank1 vec16 i == rank1 vec64 i
-  it "rank1 for BitShown (Vector Word32) and BitShown (Vector Word64) should give same answer" $ property $
-    \(Count_0_64 i) (a :: Word32) (b :: Word32) ->
+  it "rank1 for BitShown (Vector Word32) and BitShown (Vector Word64) should give same answer" $
+    forAll (choose (0, 64)) $ \(i :: Count) (a :: Word32) (b :: Word32) ->
       let a64 = fromIntegral a :: Word64 in
       let b64 = fromIntegral b :: Word64 in
       let ab64 = (b64 .<. 32) .|. a64 in
