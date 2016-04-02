88eae8339e24e712f35b58939791f5bdb07ab84c
John Ky <newhoggy@gmail.com>
2016-04-02 23:04:58 +1100

    New FromBlankedJson typeclass.  Add chunking.

--- src/HaskellWorks/Data/Json/Succinct/Cursor/BlankedJson.hs
+++ src/HaskellWorks/Data/Json/Succinct/Cursor/BlankedJson.hs
@@ -1,12 +1,19 @@
 
-module HaskellWorks.Data.Json.Succinct.Cursor.BlankedJson where
+module HaskellWorks.Data.Json.Succinct.Cursor.BlankedJson
+  ( BlankedJson(..)
+  , FromBlankedJson(..)
+  ) where
 
 import qualified Data.ByteString                      as BS
+import           HaskellWorks.Data.ByteString
 import           HaskellWorks.Data.Conduit.Json.Blank
 import           HaskellWorks.Data.Conduit.List
 import           HaskellWorks.Data.FromByteString
 
 newtype BlankedJson = BlankedJson [BS.ByteString] deriving (Eq, Show)
 
+class FromBlankedJson a where
+  fromBlankedJson :: BlankedJson -> a
+
 instance FromByteString BlankedJson where
-  fromByteString bs = BlankedJson (runListConduit blankJson [bs])
+  fromByteString bs = BlankedJson (runListConduit blankJson (bsChunkBy 4096 bs))
