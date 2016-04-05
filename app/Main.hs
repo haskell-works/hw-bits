213505d8891c3671de4c65001df6a5b03cae3515
John Ky <newhoggy@gmail.com>
2016-04-05 23:05:38 +1000

    Example code.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import qualified Data.ByteString                                  as BS
+import qualified Data.Vector.Storable                             as DVS
+import           Data.Word
+import           Foreign
+import           GHC.Conc
+import           HaskellWorks.Data.Bits.BitShown
+import           HaskellWorks.Data.FromForeignRegion
+import           HaskellWorks.Data.Json.Succinct.Cursor
+import           HaskellWorks.Data.Succinct.BalancedParens.Simple
+import           HaskellWorks.Data.Time
+import           System.IO.MMap
+
 main :: IO ()
-main = print "Hello world"
+main = do
+  (fptr :: ForeignPtr Word8, offset, size) <- mmapFileForeignPtr "/Users/jky/Downloads/78mbs.json" ReadOnly Nothing
+  !cursor <- measure (fromForeignRegion (fptr, offset, size) :: JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
+  print (jsonTokenAt (firstChild cursor))
+  threadDelay 100000000
+  print (jsonTokenAt cursor)
+  return ()
