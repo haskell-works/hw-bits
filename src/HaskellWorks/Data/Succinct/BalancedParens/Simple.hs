89f90b77d61c5ba29679b113e1ca063e87f57725
John Ky <newhoggy@gmail.com>
2016-04-08 08:14:34 +1000

    Inline some functions

--- src/HaskellWorks/Data/Succinct/BalancedParens/Simple.hs
+++ src/HaskellWorks/Data/Succinct/BalancedParens/Simple.hs
@@ -28,18 +28,22 @@ newtype SimpleBalancedParens a = SimpleBalancedParens a
 
 instance Functor SimpleBalancedParens where
   fmap f (SimpleBalancedParens a) = SimpleBalancedParens (f a)
+  {-# INLINABLE fmap   #-}
 
 instance BitShow a => Show (SimpleBalancedParens a) where
   show = bitShow
 
 closeAt :: TestBit a => a -> Count -> Bool
-closeAt v c = not (v .?. lastPositionOf c)
+closeAt v c = not (v .?. toPosition (c - 1))
+{-# INLINABLE closeAt #-}
 
 openAt :: TestBit a => a -> Count -> Bool
-openAt v c = v .?. lastPositionOf c
+openAt v c = v .?. toPosition (c - 1)
+{-# INLINABLE openAt #-}
 
 require :: Bool -> String -> a -> a
 require p msg v = if p then v else error msg
+{-# INLINABLE require #-}
 
 findOpen' :: (BitLength a, TestBit a) => Count -> SimpleBalancedParens a -> Count -> Count
 findOpen' c v p =
@@ -49,6 +53,7 @@ findOpen' c v p =
       then p
       else findOpen' (c - 1) v (p - 1)
     else findOpen' (c + 1) v (p - 1)
+{-# INLINABLE findOpen' #-}
 
 findClose' :: (BitLength a, TestBit a) => Count -> SimpleBalancedParens a -> Count -> Count
 findClose' c v p =
@@ -58,48 +63,76 @@ findClose' c v p =
       then p
       else findClose' (c + 1) v (p + 1)
     else findClose' (c - 1) v (p + 1)
+{-# INLINABLE findClose' #-}
 
 instance BalancedParens (SimpleBalancedParens [Bool]) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
 
 instance BalancedParens (SimpleBalancedParens (DVS.Vector Word8)) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
 
 instance BalancedParens (SimpleBalancedParens (DVS.Vector Word16)) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
 
 instance BalancedParens (SimpleBalancedParens (DVS.Vector Word32)) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
 
 instance BalancedParens (SimpleBalancedParens (DVS.Vector Word64)) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
 
 instance BalancedParens (SimpleBalancedParens Word8) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
 
 instance BalancedParens (SimpleBalancedParens Word16) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
 
 instance BalancedParens (SimpleBalancedParens Word32) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
 
 instance BalancedParens (SimpleBalancedParens Word64) where
   findOpen  v p = if v `openAt`  p then p else findOpen'  (Count 0) v (p - 1)
   findClose v p = if v `closeAt` p then p else findClose' (Count 0) v (p + 1)
   enclose       = findOpen' (Count 1)
+  {-# INLINABLE findOpen  #-}
+  {-# INLINABLE findClose #-}
+  {-# INLINABLE enclose   #-}
