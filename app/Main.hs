a0aec5a87ab2ec655a226ce03a0eafb2b542bc1c
John Ky <newhoggy@gmail.com>
2016-04-07 22:27:03 +1000

    Working out how much memory is actually used

--- app/Main.hs
+++ app/Main.hs
@@ -12,12 +12,39 @@ import           HaskellWorks.Data.FromByteString
 import           HaskellWorks.Data.Json.Succinct.Cursor
 import           HaskellWorks.Data.Succinct.BalancedParens.Simple
 import           HaskellWorks.Diagnostics.Time
+import           System.Mem
+
+readJson :: String -> IO (JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
+readJson path = do
+  bs <- BS.readFile path
+  print "Read file"
+  !cursor <- measure (fromByteString bs :: JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
+  print "Created cursor"
+  return cursor
 
 main :: IO ()
 main = do
-  bs <- BS.readFile "/Users/jky/Downloads/78mbs.json"
-  !cursor <- measure (fromByteString bs :: JsonCursor BS.ByteString (BitShown (DVS.Vector Word64)) (SimpleBalancedParens (DVS.Vector Word64)))
-  print (jsonTokenAt (firstChild cursor))
+  performGC
+  !c0 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c1 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c2 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c3 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c4 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c5 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c6 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c7 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c8 <- readJson "/Users/jky/Downloads/78mbs.json"
+  !c9 <- readJson "/Users/jky/Downloads/78mbs.json"
+  print "Returned from readJson"
+  performGC
   threadDelay 100000000
-  print (jsonTokenAt cursor)
-  return ()
+  print c0
+  print c1
+  print c2
+  print c3
+  print c4
+  print c5
+  print c6
+  print c7
+  print c8
+  print c9
