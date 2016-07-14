536e571d1eac554bb5879218db5c061a87d3dcab
John Ky <newhoggy@gmail.com>
2016-07-15 08:32:59 +1000

    New FromBitTextByteString typeclass

---
+++ src/HaskellWorks/Data/Bits/FromBitTextByteString.hs
@@ -0,0 +1,93 @@
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE InstanceSigs       #-}
+
+module HaskellWorks.Data.Bits.FromBitTextByteString
+    ( FromBitTextByteString(..)
+    ) where
+
+import qualified Data.ByteString      as BS
+import qualified Data.Vector.Storable as DVS
+import           Data.Word
+import           HaskellWorks.Data.Bits
+
+class FromBitTextByteString a where
+  -- | Convert a binary byte string to a value of type @a
+  fromBitTextByteString :: BS.ByteString -> a
+
+instance FromBitTextByteString (DVS.Vector Word8) where
+  fromBitTextByteString :: BS.ByteString -> DVS.Vector Word8
+  fromBitTextByteString bs = DVS.unfoldrN (BS.length bs `div` 8 + 1) gen bs
+    where gen :: BS.ByteString -> Maybe (Word8, BS.ByteString)
+          gen cs = case BS.uncons cs of
+            Just (d, ds) | d == w0  -> gen' 1 0 ds
+            Just (d, ds) | d == w1  -> gen' 1 1 ds
+            Just (_, ds)            -> gen ds
+            Nothing                 -> Nothing
+          gen' :: Int -> Word8 -> BS.ByteString -> Maybe (Word8, BS.ByteString)
+          gen' n w cs
+            | n >= 8   = Just (w, cs)
+            | otherwise = case BS.uncons cs of
+                Just (d, ds) | d == w0  -> gen' (n + 1) (w .|. (0 .<. fromIntegral n)) ds
+                Just (d, ds) | d == w1  -> gen' (n + 1) (w .|. (1 .<. fromIntegral n)) ds
+                Just (_, ds)            -> gen' n w ds
+                Nothing                 -> Just (w, cs)
+
+instance FromBitTextByteString (DVS.Vector Word16) where
+  fromBitTextByteString :: BS.ByteString -> DVS.Vector Word16
+  fromBitTextByteString bs = DVS.unfoldrN (BS.length bs `div` 16 + 1) gen bs
+    where gen :: BS.ByteString -> Maybe (Word16, BS.ByteString)
+          gen cs = case BS.uncons cs of
+            Just (d, ds) | d == w0  -> gen' 1 0 ds
+            Just (d, ds) | d == w1  -> gen' 1 1 ds
+            Just (_, ds)            -> gen ds
+            Nothing                 -> Nothing
+          gen' :: Int -> Word16 -> BS.ByteString -> Maybe (Word16, BS.ByteString)
+          gen' n w cs
+            | n >= 16   = Just (w, cs)
+            | otherwise = case BS.uncons cs of
+                Just (d, ds) | d == w0  -> gen' (n + 1) (w .|. (0 .<. fromIntegral n)) ds
+                Just (d, ds) | d == w1  -> gen' (n + 1) (w .|. (1 .<. fromIntegral n)) ds
+                Just (_, ds)            -> gen' n w ds
+                Nothing                 -> Just (w, cs)
+
+instance FromBitTextByteString (DVS.Vector Word32) where
+  fromBitTextByteString :: BS.ByteString -> DVS.Vector Word32
+  fromBitTextByteString bs = DVS.unfoldrN (BS.length bs `div` 32 + 1) gen bs
+    where gen :: BS.ByteString -> Maybe (Word32, BS.ByteString)
+          gen cs = case BS.uncons cs of
+            Just (d, ds) | d == w0  -> gen' 1 0 ds
+            Just (d, ds) | d == w1  -> gen' 1 1 ds
+            Just (_, ds)            -> gen ds
+            Nothing                 -> Nothing
+          gen' :: Int -> Word32 -> BS.ByteString -> Maybe (Word32, BS.ByteString)
+          gen' n w cs
+            | n >= 32   = Just (w, cs)
+            | otherwise = case BS.uncons cs of
+                Just (d, ds) | d == w0  -> gen' (n + 1) (w .|. (0 .<. fromIntegral n)) ds
+                Just (d, ds) | d == w1  -> gen' (n + 1) (w .|. (1 .<. fromIntegral n)) ds
+                Just (_, ds)            -> gen' n w ds
+                Nothing                 -> Just (w, cs)
+
+instance FromBitTextByteString (DVS.Vector Word64) where
+  fromBitTextByteString :: BS.ByteString -> DVS.Vector Word64
+  fromBitTextByteString bs = DVS.unfoldrN (BS.length bs `div` 64 + 1) gen bs
+    where gen :: BS.ByteString -> Maybe (Word64, BS.ByteString)
+          gen cs = case BS.uncons cs of
+            Just (d, ds) | d == w0  -> gen' 1 0 ds
+            Just (d, ds) | d == w1  -> gen' 1 1 ds
+            Just (_, ds)            -> gen ds
+            Nothing                 -> Nothing
+          gen' :: Int -> Word64 -> BS.ByteString -> Maybe (Word64, BS.ByteString)
+          gen' n w cs
+            | n >= 64   = Just (w, cs)
+            | otherwise = case BS.uncons cs of
+                Just (d, ds) | d == w0  -> gen' (n + 1) (w .|. (0 .<. fromIntegral n)) ds
+                Just (d, ds) | d == w1  -> gen' (n + 1) (w .|. (1 .<. fromIntegral n)) ds
+                Just (_, ds)            -> gen' n w ds
+                Nothing                 -> Just (w, cs)
+
+w0 :: Word8
+w0 = 48
+
+w1 :: Word8
+w1 = 49
---
+++ test/HaskellWorks/Data/Bits/FromBitTextByteStringSpec.hs
@@ -0,0 +1,94 @@
+{-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
+
+{-# LANGUAGE OverloadedLists     #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module HaskellWorks.Data.Bits.FromBitTextByteStringSpec (spec) where
+
+import qualified Data.ByteString                              as BS
+import qualified Data.Vector.Storable                         as DVS
+import           Data.Word
+import           HaskellWorks.Data.Bits.FromBitTextByteString
+import           Test.Hspec
+
+{-# ANN module ("HLint: Ignore Redundant do" :: String) #-}
+
+spec :: Spec
+spec = describe "HaskellWorks.Data.FromBitTextByteStringSpec" $ do
+  describe "For (DVS.Vector Word8)" $ do
+    it "fromBitTextByteString (BS.unpack []) :: DVS.Vector Word8" $
+      let w = fromBitTextByteString (BS.pack []) ::DVS.Vector Word8 in
+      w `shouldBe` DVS.fromList []
+    it "fromBitTextByteString (BS.unpack \"0\") :: DVS.Vector Word8" $
+      let w = fromBitTextByteString "0" ::DVS.Vector Word8 in
+      w `shouldBe` DVS.fromList [0x0]
+    it "fromBitTextByteString (BS.unpack \"1\") :: DVS.Vector Word8" $
+      let w = fromBitTextByteString "1" ::DVS.Vector Word8 in
+      w `shouldBe` DVS.fromList [0x1]
+    it "fromBitTextByteString (BS.unpack \"11110000\") :: DVS.Vector Word8" $
+      let w = fromBitTextByteString "11110000" ::DVS.Vector Word8 in
+      w `shouldBe` DVS.fromList [0x0f]
+    it "fromBitTextByteString (BS.unpack \"111100000\") :: DVS.Vector Word8" $
+      let w = fromBitTextByteString "111100000" ::DVS.Vector Word8 in
+      w `shouldBe` DVS.fromList [0x0f, 0x0]
+    it "fromBitTextByteString (BS.unpack \"111100001\") :: DVS.Vector Word8" $
+      let w = fromBitTextByteString "111100001" ::DVS.Vector Word8 in
+      w `shouldBe` DVS.fromList [0x0f, 0x1]
+  describe "For (DVS.Vector Word16)" $ do
+    it "fromBitTextByteString (BS.unpack []) :: DVS.Vector Word16" $
+      let w = fromBitTextByteString (BS.pack []) ::DVS.Vector Word16 in
+      w `shouldBe` DVS.fromList []
+    it "fromBitTextByteString (BS.unpack \"0\") :: DVS.Vector Word16" $
+      let w = fromBitTextByteString "0" ::DVS.Vector Word16 in
+      w `shouldBe` DVS.fromList [0x0]
+    it "fromBitTextByteString (BS.unpack \"1\") :: DVS.Vector Word16" $
+      let w = fromBitTextByteString "1" ::DVS.Vector Word16 in
+      w `shouldBe` DVS.fromList [0x1]
+    it "fromBitTextByteString (BS.unpack \"1111000011110000\") :: DVS.Vector Word16" $
+      let w = fromBitTextByteString "1111000011110000" ::DVS.Vector Word16 in
+      w `shouldBe` DVS.fromList [0x0f0f]
+    it "fromBitTextByteString (BS.unpack \"11110000111100000\") :: DVS.Vector Word16" $
+      let w = fromBitTextByteString "11110000111100000" ::DVS.Vector Word16 in
+      w `shouldBe` DVS.fromList [0x0f0f, 0x0]
+    it "fromBitTextByteString (BS.unpack \"11110000111100001\") :: DVS.Vector Word16" $
+      let w = fromBitTextByteString "11110000111100001" ::DVS.Vector Word16 in
+      w `shouldBe` DVS.fromList [0x0f0f, 0x1]
+  describe "For (DVS.Vector Word32)" $ do
+    it "fromBitTextByteString (BS.unpack []) :: DVS.Vector Word32" $
+      let w = fromBitTextByteString (BS.pack []) ::DVS.Vector Word32 in
+      w `shouldBe` DVS.fromList []
+    it "fromBitTextByteString (BS.unpack \"0\") :: DVS.Vector Word32" $
+      let w = fromBitTextByteString "0" ::DVS.Vector Word32 in
+      w `shouldBe` DVS.fromList [0x0]
+    it "fromBitTextByteString (BS.unpack \"1\") :: DVS.Vector Word32" $
+      let w = fromBitTextByteString "1" ::DVS.Vector Word32 in
+      w `shouldBe` DVS.fromList [0x1]
+    it "fromBitTextByteString (BS.unpack \"11110000111100001111000011110000\") :: DVS.Vector Word32" $
+      let w = fromBitTextByteString "11110000111100001111000011110000" ::DVS.Vector Word32 in
+      w `shouldBe` DVS.fromList [0x0f0f0f0f]
+    it "fromBitTextByteString (BS.unpack \"111100001111000011110000111100000\") :: DVS.Vector Word32" $
+      let w = fromBitTextByteString "111100001111000011110000111100000" ::DVS.Vector Word32 in
+      w `shouldBe` DVS.fromList [0x0f0f0f0f, 0x0]
+    it "fromBitTextByteString (BS.unpack \"111100001111000011110000111100001\") :: DVS.Vector Word32" $
+      let w = fromBitTextByteString "111100001111000011110000111100001" ::DVS.Vector Word32 in
+      w `shouldBe` DVS.fromList [0x0f0f0f0f, 0x1]
+  describe "For (DVS.Vector Word64)" $ do
+    it "fromBitTextByteString (BS.unpack []) :: DVS.Vector Word64" $
+      let w = fromBitTextByteString (BS.pack []) ::DVS.Vector Word64 in
+      w `shouldBe` DVS.fromList []
+    it "fromBitTextByteString (BS.unpack \"0\") :: DVS.Vector Word64" $
+      let w = fromBitTextByteString "0" ::DVS.Vector Word64 in
+      w `shouldBe` DVS.fromList [0x0]
+    it "fromBitTextByteString (BS.unpack \"1\") :: DVS.Vector Word64" $
+      let w = fromBitTextByteString "1" ::DVS.Vector Word64 in
+      w `shouldBe` DVS.fromList [0x1]
+    it "fromBitTextByteString (BS.unpack \"1111000011110000111100001111000011110000111100001111000011110000\") :: DVS.Vector Word64" $
+      let w = fromBitTextByteString "1111000011110000111100001111000011110000111100001111000011110000" ::DVS.Vector Word64 in
+      w `shouldBe` DVS.fromList [0x0f0f0f0f0f0f0f0f]
+    it "fromBitTextByteString (BS.unpack \"11110000111100001111000011110000111100001111000011110000111100000\") :: DVS.Vector Word64" $
+      let w = fromBitTextByteString "11110000111100001111000011110000111100001111000011110000111100000" ::DVS.Vector Word64 in
+      w `shouldBe` DVS.fromList [0x0f0f0f0f0f0f0f0f, 0x0]
+    it "fromBitTextByteString (BS.unpack \"11110000111100001111000011110000111100001111000011110000111100001\") :: DVS.Vector Word64" $
+      let w = fromBitTextByteString "11110000111100001111000011110000111100001111000011110000111100001" ::DVS.Vector Word64 in
+      w `shouldBe` DVS.fromList [0x0f0f0f0f0f0f0f0f, 0x1]
