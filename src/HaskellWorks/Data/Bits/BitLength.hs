36333179c6c6e077f1431ef0c0305003f1bfddf6
John Ky <newhoggy@gmail.com>
2016-08-15 06:50:25 +1000

    Inline

--- src/HaskellWorks/Data/Bits/BitLength.hs
+++ src/HaskellWorks/Data/Bits/BitLength.hs
@@ -34,81 +34,83 @@ class BitLength v where
 
 elemBitLength :: (VectorLike v, BitLength (Elem v)) => v -> Count
 elemBitLength v = bitLength (v !!! 0)
+{-# INLINE elemBitLength #-}
 
 elemBitEnd :: (VectorLike v, BitLength (Elem v)) => v -> Position
 elemBitEnd v = endPosition (v !!! 0)
+{-# INLINE elemBitEnd #-}
 
 --------------------------------------------------------------------------------
 -- Instances
 
 instance BitLength Bool where
   bitLength _ = 1
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength [Bool] where
   bitLength = fromIntegral . P.length
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength Word8 where
   bitLength _ = 8
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength Word16 where
   bitLength _ = 16
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength Word32 where
   bitLength _ = 32
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength Word64 where
   bitLength _ = 64
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength [Word8] where
   bitLength v = fromIntegral (P.length v) * bitLength (head v)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength [Word16] where
   bitLength v = fromIntegral (P.length v) * bitLength (head v)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength [Word32] where
   bitLength v = fromIntegral (P.length v) * bitLength (head v)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength [Word64] where
   bitLength v = fromIntegral (P.length v) * bitLength (head v)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength (DV.Vector Word8) where
   bitLength v = vLength v * bitLength (v !!! 0)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength (DV.Vector Word16) where
   bitLength v = vLength v * bitLength (v !!! 0)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength (DV.Vector Word32) where
   bitLength v = vLength v * bitLength (v !!! 0)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength (DV.Vector Word64) where
   bitLength v = vLength v * bitLength (v !!! 0)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength (DVS.Vector Word8) where
   bitLength v = vLength v * bitLength (v !!! 0)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength (DVS.Vector Word16) where
   bitLength v = vLength v * bitLength (v !!! 0)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength (DVS.Vector Word32) where
   bitLength v = vLength v * bitLength (v !!! 0)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
 
 instance BitLength (DVS.Vector Word64) where
   bitLength v = vLength v * bitLength (v !!! 0)
-  {-# INLINABLE bitLength #-}
+  {-# INLINE bitLength #-}
