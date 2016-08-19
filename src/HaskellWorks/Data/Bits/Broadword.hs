4aeeac4c1093ece2d52b64ac3cb2bffdb4fd1a00
John Ky <newhoggy@gmail.com>
2016-08-19 19:22:04 +1000

    Remove unused code.

--- src/HaskellWorks/Data/Bits/Broadword.hs
+++ src/HaskellWorks/Data/Bits/Broadword.hs
@@ -4,26 +4,16 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE BangPatterns          #-}
 
-
 -- |
 -- Copyright: 2016 John Ky
 -- License: MIT
 --
 -- Succinct operations.
 module HaskellWorks.Data.Bits.Broadword
-  ( mu
-  , h
+  ( h
   , l
   , kBitDiff
   , kBitDiffSimple
-  , kBitNonZero
-  , kBitDiffPos
-  , k8BitSidewaysAdd
-  , k8BitExcesses
-  , k8BitUpdateMask
-  , k8BitZ
-  , k8BitUpdateB
-  , k8BitUpdateZ
   , findCloseW64
   ) where
 
@@ -31,11 +21,7 @@ import qualified Data.Bits                        as DB
 import           Data.Word
 import           HaskellWorks.Data.Bits.BitShown
 import           HaskellWorks.Data.Bits.BitWise
-import           HaskellWorks.Data.Positioning
 import           Debug.Trace
-
-mu :: Count -> Word64
-mu k = (maxBound :: Word64) `div` ((1 .<. fromIntegral ((1 :: Word64) .<. k)) + 1)
 
 l :: Int -> Word64
 l 2   = 0x5555555555555555
@@ -45,6 +31,7 @@ l 16  = 0x0001000100010001
 l 32  = 0x0000000100000001
 l 64  = 0x0000000000000001
 l k   = error ("Invalid h k where k = " ++ show k)
+{-# INLINE l #-}
 
 h :: Int -> Word64
 h 2   = 0xaaaaaaaaaaaaaaaa
@@ -54,54 +41,17 @@ h 16  = 0x8000800080008000
 h 32  = 0x8000000080000000
 h 64  = 0x8000000000000000
 h k   = error ("Invalid h k where k = " ++ show k)
+{-# INLINE h #-}
 
 -- Section 3 Eq 1
 kBitDiff :: Int -> Word64 -> Word64 -> Word64
 kBitDiff k x y = ((x .|. h k) - (y .&. comp (h k))) .^. ((x .^. comp y) .&. h k)
+{-# INLINE kBitDiff #-}
 
 -- Section 3 Eq 2
 kBitDiffSimple :: Int -> Word64 -> Word64 -> Word64
 kBitDiffSimple k x y = ((x .|. h k) - y) .^. h k
-
--- Section 3 Eq 3
-kBitNonZero :: Int -> Word64 -> Word64
-kBitNonZero k x =  ((x .|. h k) - l k) .|. x  .&. h k
-
--- Section 3 Eq 4
-kBitDiffPos :: Int -> Word64 -> Word64 -> Word64
-kBitDiffPos k x y = kBitDiff k x y .&. (kBitDiff k x y .>. (fromIntegral k - 1)) - 1
-
--- Section 4 Eq 2
-k8BitSidewaysAdd :: Word64 -> Word64
-k8BitSidewaysAdd x =
-  let b = (x - (x .&. 0xaaaaaaaaaaaaaaaa)) .>. 1 in
-  let c = (b .&. 0x3333333333333333) + ((b .>. 2) .&. 0x3333333333333333) in
-  let d = (c + (c .>. 4)) .&. 0x0f0f0f0f0f0f0f0f in
-  let e = (d * 0x0101010101010101) .<. 1 in
-  e
-
--- Section 4 Eq 6
-k8BitExcesses :: Word64 -> Word64
-k8BitExcesses x =
-  let b = k8BitSidewaysAdd x in
-  let c = kBitDiff 8 (h 8 .|. 0x4038302820181008) b in
-  c
-
--- Section 4 Eq 7
-k8BitUpdateMask :: Word64 -> Word64
-k8BitUpdateMask x = ((((x .|. h 8) - l 8) .>. 7 .&. l 8) .|. h 8) - l 8
-
--- Section 4 Eq 8
-k8BitZ :: Word64 -> Word64
-k8BitZ u = (h 8 .>. 1 .|. l 8 * 7) .&. u
-
-k8BitUpdateB :: Word64 -> Word64
-k8BitUpdateB b = b - (l 8 * 2 - ((b .>. 6 .&. l 8 .<. 1) + (b .>. 5 .&. l 8 .<. 1)))
-
-k8BitUpdateZ :: Word64 -> Word64 -> Word64
-k8BitUpdateZ z u = z .&. comp u .|. (h 8 .>. 1 .|. l 8 * 5) .&. u
-
--- countTrailingZeros :: b -> Int
+{-# INLINE kBitDiffSimple #-}
 
 traceW :: String -> Word64 -> Word64
 traceW s w = trace (s ++ ": " ++ show (BitShown w) ++ " : " ++ show w) w
@@ -126,7 +76,7 @@ findCloseW64 x = let !_ = traceW "x00" x in
   let !z20 = (z10 .&. comp u20) .|. (((h 8 .>. 1) .|. (l 8 * 3)) .&. u20)           in let !_ = traceW "z20" z20 in
   let !b30 = b10 - (l 8 * 2 - ((x .>. 2 .&. l 8 .<. 1) + (x .>. 1 .&. l 8 .<. 1)))  in let !_ = traceW "b30" b30 in
   let !u30 = (((((b30 .|. h 8) - l 8) .>. 7) .&. l 8) .|. h 8) - l 8                in let !_ = traceW "u30" u30 in
-  let !z30 = (z20 .&. comp u30) .|. (((h 8 .>. 1) .|. (l 8 * 1)) .&. u30)            in let !_ = traceW "z30" z30 in
+  let !z30 = (z20 .&. comp u30) .|. (((h 8 .>. 1) .|.  l 8     ) .&. u30)            in let !_ = traceW "z30" z30 in
   let !p00 = lsb (z30 .>. 6 .&. l 8)                                                in
   let !(r00 :: Word64) = ((p00 + (((z30 :: Word64) .>. fromIntegral p00) .&. 0x3f)) .|. (p00 .>. 8)) .&. 0x7f in let !_ = traceW "r00" r00 in
   r00
