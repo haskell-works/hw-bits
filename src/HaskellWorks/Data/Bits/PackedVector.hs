5cd97fa126e526ed6f99c5ae30736827387525dc
John Ky <newhoggy@gmail.com>
2016-10-28 20:19:36 +1100

    Remove PackedVector module.  This has been moved to hw-packed-vector package

--- src/HaskellWorks/Data/Bits/PackedVector.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
-
-module HaskellWorks.Data.Bits.PackedVector
-    ( module X
-    ) where
-
-import           HaskellWorks.Data.Bits.PackedVector.Internal       as X
-import           HaskellWorks.Data.Bits.PackedVector.PackedVector64 as X
