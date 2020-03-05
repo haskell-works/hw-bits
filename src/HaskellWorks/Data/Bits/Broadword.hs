9f72080341cf064bd9bb801d0bc8d5a2a5b34fdd
John Ky <newhoggy@gmail.com>
2020-03-05 22:29:47 +1100

    Document the lsb function

--- src/HaskellWorks/Data/Bits/Broadword.hs
+++ src/HaskellWorks/Data/Bits/Broadword.hs
@@ -62,6 +62,17 @@ kBitDiffUnsafe :: Int -> Word64 -> Word64 -> Word64
 kBitDiffUnsafe k x y = ((x .|. h k) - y) .^. h k
 {-# INLINE kBitDiffUnsafe #-}
 
+-- | Returns the position of the least significant bit (0-based)
+--
+-- This is equivalent to 'DB.countTrailingZeros' except for when there are no bits set.  In which case
+-- return a word with all bits set.
+--
+-- >>> lsb 8
+-- 3
+-- >>> lsb 1
+-- 0
+-- >>> lsb 0
+-- 18446744073709551615
 lsb :: Word64 -> Word64
 lsb w = let r = fromIntegral (DB.countTrailingZeros w) in r .|. negate ((r .>. 6) .&. 0x1)
 {-# INLINE lsb #-}
